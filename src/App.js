46352c9d260e1ead22279840c1699705d43144ce
scarl-ett <chdnjs7610@gmail.com>
2022-04-17 20:21:38 +0900

    event, ref

--- src/App.js
+++ src/App.js
@@ -94,12 +94,29 @@
 
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
-import Say from './Say';
+// import Say from './Say';
+// import EventPractice from './EventPractice';
 
-const App = () => {
-  // return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent >;
-  // return <Counter />
-  return <Say />
-};
+// const App = () => {
+//   // return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent >;
+//   // return <Counter />
+//   // return <Say />
+//   // return <EventPractice />
+// };
+
+import React, { Component } from 'react';
+// import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+class App extends Component {
+  render() {
+    return (
+      // <ValidationSample />
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox = ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
+}
 
 export default App;

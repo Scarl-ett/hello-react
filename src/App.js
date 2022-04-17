69eac7afafdad4298298ddffb08e1593ec41c47c
scarl-ett <chdnjs7610@gmail.com>
2022-04-17 22:15:48 +0900

    컴포넌트 반복

--- src/App.js
+++ src/App.js
@@ -106,15 +106,17 @@
 
 import React, { Component } from 'react';
 // import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
+// import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 class App extends Component {
   render() {
     return (
       // <ValidationSample />
-      <div>
-        <ScrollBox ref={(ref) => this.scrollBox = ref} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
-      </div>
+      // <div>
+      //   <ScrollBox ref={(ref) => this.scrollBox = ref} />
+      //   <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      // </div>
+      <IterationSample />
     );
   }
 }
